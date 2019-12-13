d362f19c36534d5816a6f76310e1e103979ee577
FuzzJS <adanuriplata@gmail.com>
2019-12-12 18:27:57 -0600

    Se elimina top-header con info de cuentas

--- templates/header-style3.php
+++ templates/header-style3.php
@@ -6,56 +6,6 @@ $himarket_page_hotdeals = himarket_options()->getCpanelValue('hot_deals');
 ?>
 
 
-<!-- START cnk hide top bar accoun menu user -->
-
-
-<div class="yt-header-top">
-	<div class="container">
-		<div class="row">
-		    <div class="col-lg-3 col-md-4 col-sm-4 col-xs-5 sl-header-login">
-		    	<?php get_template_part('widgets/himarket_top/login-header1'); ?>
-		    </div>
-		    <div class="header-right col-lg-9 col-md-8 col-sm-8 col-xs-7">
-		        <div class="border-header">
-				<!-- LANGUAGE_CURENCY -->
-					<div class="top-links-action">
-					<div class="phone pull-left">
-					<?php if($himarket_my_phone != '') {?>
-								    <i class="sp-ic fa fa-phone">&nbsp;</i><?php esc_html_e('Phone:','himarket') ;?> <a title="<?php echo esc_attr( $himarket_my_phone ) ?>" href="#"><?php echo esc_html( $himarket_my_phone ) ?></a> 
-								<?php } ?>
-					</div>
-						<?php if (is_active_sidebar_HIMARKET('top')) {?>
-								<?php dynamic_sidebar('top'); ?>
-						<?php }?>
-						
-						<div class="my-account pull-right">
-						    <ul>
-						        <li>
-						            <a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>"><?php esc_html_e('My Account', 'himarket'); ?></a>
-						        	<ul>
-												<li><a href="<?php echo get_permalink( get_option('woocommerce_myaccount_page_id') ); ?>" title="<?php esc_html_e('My Account','himarket'); ?>"><?php esc_html_e('My Account','himarket'); ?></a></li>
-												<?php 
-													if( function_exists( 'YITH_WCWL' ) ){
-														$wishlist_url = YITH_WCWL()->get_wishlist_url(); 															
-													?>
-												<li><a href="<?php echo esc_url( $wishlist_url ) ?>" title="<?php esc_html_e('My Wishlist','himarket'); ?>"><?php esc_html_e('My Wishlist', 'himarket'); ?></a></li>
-												<?php } ?>
-												<li><a href="<?php echo get_permalink(get_option('woocommerce_cart_page_id')); ?>" title="<?php esc_attr_e( 'Cart', 'himarket' ) ?>"><?php esc_html_e('My Cart', 'himarket'); ?></a></li>
-												<li><a href="<?php echo get_permalink(get_option('woocommerce_checkout_page_id')); ?>" title="<?php esc_attr_e( 'Check Out', 'himarket' ) ?>"><?php esc_html_e('Checkout', 'himarket'); ?></a></li>
-							        </ul>
-						        </li>
-						        
-						    </ul>
-						</div>
-					</div>
-			    </div>
-			</div>
-		</div>
-	</div>
-</div>
-
-<!-- END cnk hide top bar accoun menu user -->
-
 <div class="yt-header-middle">
 	<div class="container">
 		<div class="row">
